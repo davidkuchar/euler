81050e7b1e40b2ce3e22bb1140b5a637332fbcc6
davidkuchar <davidkuchar@gmail.com>
2013-04-30 16:28:52 -0700

    removed some notes.

--- solutions.rb
+++ solutions.rb
@@ -1,10 +1,9 @@
-
-# ruby motion
-# sferik@gmail.com
-
 require 'Prime'
 require 'matrix'
 require 'benchmark'
+
+if __FILE__ == $0
+end
 
 def run(&problem)
 	result = ""
@@ -424,17 +423,20 @@ def p13
 	sum.to_s[0..9]
 end
 
-
 # Longest Collatz sequence
 # 	Problem 14
 # 	The following iterative sequence is defined for the set of positive integers:
-# 	n  n/2 (n is even)
-# 	n  3n + 1 (n is odd)
+# 	n -> n/2 (n is even)
+# 	n -> 3n + 1 (n is odd)
 # 	Using the rule above and starting with 13, we generate the following sequence:
-# 	13  40  20  10  5  16  8  4  2  1
+# 	13 -> 40 -> 20 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
 # 	It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
 # 	Which starting number, under one million, produces the longest chain?
 # 	NOTE: Once the chain starts the terms are allowed to go above one million.
+def next_collatz(n)
+	n / 2 if n.even? else 3 * n + 1
+end
+
 def p14
 
 
