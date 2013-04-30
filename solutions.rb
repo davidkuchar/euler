a00db3d811e4c8d37d0233488e04d12caa363f7e
davidkuchar <davidkuchar@gmail.com>
2013-04-30 16:16:37 -0700

    solved p13

--- solutions.rb
+++ solutions.rb
@@ -419,7 +419,23 @@ def p13
 		20849603980134001723930671666823555245252804609722,
 		53503534226472524250874054075591789781264330331690,
 	]
-	numbers.each do |x|
 
-	end
+	sum = numbers.reduce(:+)
+	sum.to_s[0..9]
+end
+
+
+# Longest Collatz sequence
+# 	Problem 14
+# 	The following iterative sequence is defined for the set of positive integers:
+# 	n  n/2 (n is even)
+# 	n  3n + 1 (n is odd)
+# 	Using the rule above and starting with 13, we generate the following sequence:
+# 	13  40  20  10  5  16  8  4  2  1
+# 	It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+# 	Which starting number, under one million, produces the longest chain?
+# 	NOTE: Once the chain starts the terms are allowed to go above one million.
+def p14
+
+
 end
